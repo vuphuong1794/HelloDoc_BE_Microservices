30029fc8c9261153ae21dddbd4b007b49766df77
Phuc Le <lenguyenminhphuc0706@gmail.com>
2025-11-25 16:14:42 +0700

    remove unnecessary model

--- apps/news-favorite/src/service/news-favorite.service.ts
+++ apps/news-favorite/src/service/news-favorite.service.ts
@@ -4,19 +4,11 @@ import { Model } from 'mongoose';
 import { NewsFavorite } from '../core/schema/news-favorite.schema';
 import { CreateNewsFavoriteDto } from '../core/dto/create-news-favorite.dto';
 import { GetNewsFavoriteDto } from '../core/dto/get-news-favorite.dto';
-import { News } from 'apps/news/src/core/schema/news.schema';
-import { User } from 'apps/users/src/core/schema/user.schema';
-import { Doctor } from 'apps/doctor/src/core/schema/doctor.schema';
-import { Admin } from 'apps/admin/src/core/schema/admin.schema';
 
 @Injectable()
 export class NewsFavoriteService {
   constructor(
     @InjectModel(NewsFavorite.name, 'newsFavoriteConnection') private newsFavoriteModel: Model<NewsFavorite>,
-    @InjectModel(News.name, 'newsFavoriteConnection') private newsModel: Model<News>,
-    @InjectModel(User.name, 'newsFavoriteConnection') private userModel: Model<User>,
-    @InjectModel(Doctor.name, 'newsFavoriteConnection') private doctorModel: Model<Doctor>,
-    @InjectModel(Admin.name, 'newsFavoriteConnection') private adminModel: Model<Admin>,
   ) {}
 
   async getNewsFavoritesByNewsId(newsId: string, dto: GetNewsFavoriteDto) {
