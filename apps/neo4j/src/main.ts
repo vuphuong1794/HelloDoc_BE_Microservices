3ae797dad2b5420339dd55e37c942fae2c395e5c
vuphuong1794 <phuongvu1794@gmail.com>
2025-12-05 15:23:45 +0700

    refactor

--- apps/neo4j/src/main.ts
+++ apps/neo4j/src/main.ts
@@ -3,24 +3,14 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { Neo4jModule } from './use-case/neo4j.module';
 
 async function bootstrap() {
-  const port = 3038
-
-  // Tạo HTTP application (để Render detect được)
-  const app = await NestFactory.create(Neo4jModule);
-
-  // Thêm TCP microservice nếu cần
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      host: '0.0.0.0',
-      port: 3008, // TCP port khác với HTTP port
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    Neo4jModule,
+    {
+      transport: Transport.TCP,
+      options: { port: 3008 }, // khác với 3004 của News
     },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`HTTP server listening on port 3038`);
-  console.log(`TCP microservice listening on port 3008`);
+  );
+  await app.listen();
+  console.log('Neo4j service is listening on port 3008');
 }
 bootstrap();
--- apps/nlp-integration/src/main.ts
+++ apps/nlp-integration/src/main.ts
@@ -1,26 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as dotenv from 'dotenv';
 import { NlpIntegrationModule } from './use-case/nlp-integration.module';
 
 async function bootstrap() {
-  const port = 3060
+  dotenv.config();
 
-  // Tạo HTTP application (để Render detect được)
-  const app = await NestFactory.create(NlpIntegrationModule);
-
-  // Thêm TCP microservice nếu cần
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      host: '0.0.0.0',
-      port: 3022, // TCP port khác với HTTP port
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NlpIntegrationModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3022,
+      },
     },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`HTTP server listening on port 3060`);
-  console.log(`TCP microservice listening on port 3022`);
+  );
+  await app.listen();
+  console.log('NLP intergration service is listening on port 3022');
 }
 bootstrap();
