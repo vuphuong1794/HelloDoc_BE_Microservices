1e3664d2cf6db9a48e77769724252b2c69124030
vuphuong1794 <phuongvu1794@gmail.com>
2026-02-27 15:53:23 +0700

    add port

--- apps/specialty/src/main.ts
+++ apps/specialty/src/main.ts
@@ -2,22 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { SpecialtyModule } from './use-case/specialty.module';
 
-
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    SpecialtyModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqps://udjevvyv:fJMVuL7NXdi1cHx42OZAXRRLjYnPX3os@campbell.lmq.cloudamqp.com/udjevvyv'],
-        queue: 'specialty_queue',
-        queueOptions: {
-          durable: true
-        },
+  // HTTP app để Render detect port
+  const app = await NestFactory.create(SpecialtyModule);
+  const port = 3009;
+
+  // RMQ Microservice
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RMQ_URL || 'amqps://udjevvyv:fJMVuL7NXdi1cHx42OZAXRRLjYnPX3os@campbell.lmq.cloudamqp.com/udjevvyv'],
+      queue: 'specialty_queue',
+      queueOptions: {
+        durable: true,
       },
     },
-  );
-  await app.listen();
-  console.log('Specialty service is listening on port 3009');
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(port);
+  console.log(`Specialty service is listening on port ${port}`);
 }
 bootstrap();
